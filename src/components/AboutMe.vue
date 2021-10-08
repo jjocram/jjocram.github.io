e23ca54d215c036dd8c556532db79b7e66e9c504
Marco Ferrati <marco.ferrati@gmail.com>
2021-10-08 15:39:45 +0200

    Fix typo

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -2,7 +2,7 @@
   <section class="section has-text-centered">
     <h1 class="title">About me</h1>
     <h2 class="subtitle">
-      My name is Marco Ferrati, a MSc student in Computer Science at the University of Bologna.
+      My name is Marco Ferrati, an MSc student in Computer Science at the University of Bologna.
     </h2>
     <p>My classic nickname is jjocram and my classic profile image is the one with the dog. So, if online have you ever seen an user with the name jjocram and the profile pic as that dog it was me :)</p>
   </section>
