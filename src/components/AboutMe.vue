33cc4cfe4f25184cba6f3b52c137ca741657402a
Marco Ferrati <marco.ferrati@gmail.com>
2022-05-04 09:06:12 +0200

    Update UI

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,9 +1,25 @@
 <template>
   <section class="section has-text-centered">
     <h1 class="title">About me</h1>
-    <h2 class="subtitle">
-      My name is Marco Ferrati, an MSc student in Computer Science at the University of Bologna.
-    </h2>
+    <table class="table">
+      <tbody>
+        <tr>
+          <td class="presentation-cell">
+            <h2 class="subtitle">My name is Marco Ferrati,</h2>
+          </td>
+          <td class="presentation-cell">
+            <h2 class="subtitle">an MSc student in Computer Science at the University of Bologna.</h2>
+          </td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td class="presentation-cell">
+            <h2 class="subtitle past">a Bachelor's student in Computer Science at the University of Padova.</h2>
+          </td>
+        </tr>
+      </tbody>
+    </table>
     <p>My classic nickname is jjocram and, my classic profile image is the one with the dog. So, if online have you ever seen a user with the name jjocram and the profile pic as that dog, it was me :)</p>
   </section>
 </template>
