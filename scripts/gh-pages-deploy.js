1277a85c6eaa7f0ee473438d4df805a48ced0c1b
Marco Ferrati <marco.ferrati@gmail.com>
2022-09-24 20:20:37 +0200

    ci: fix import and execa invocation

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,20 +1,20 @@
 (async () => {
-const execa = await import ("execa");
-const fs = await import("fs");
+  const execa = await import('execa');
+  const fs = await import('fs');
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    await execa.execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
-    await execa("npm", ["run", "build"]);
+    await execa.execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
-    await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    await execa.execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa.execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("rm", ["-r", folderName]);
-    await execa("git", ["checkout", "-f", "master"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
+    await execa.execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa.execa("rm", ["-r", folderName]);
+    await execa.execa("git", ["checkout", "-f", "master"]);
+    await execa.execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
     // eslint-disable-next-line no-console
